2c89bdeb606aa3af6c88863854b38fbc0e17134d
Ion Toloaca <(none)>
2016-04-12 01:12:41 +0200

    Remove /get_arguments post request

--- app.pl
+++ app.pl
@@ -92,81 +92,15 @@ post '/detect_notations' => sub {
   print "Length of input: $length \n";
   my $start = 0; #default - zero
   my $pos = $recce->read( \"$input", $start, $length);
-  my $actual_events = {};
-
-  #Record positions where notations matched#######################################122
-  READ: while (1) {
-    my @current_events = ();
-    
-    EVENT:
-    for my $event ( @{ $recce->events() } ) {
-      my ($name) = @{$event};
-      push @current_events, $name;
-    }
-
-    for (@current_events) {
-        my ($start_rule, $length_rule) = $recce->last_completed($_);
-        my $last_expression = $recce->substring($start_rule, $length_rule);
-        # push @$actual_events, [$pos - length($last_expression),length($last_expression), $_] if ($_ !~ /^argRuleN.*/);
-        if ($_ !~ /^argRuleN.*/) {
-          my $str = encodeURIComponent(substr($global_input, $pos - length($last_expression), length($last_expression)));
-          push @{$actual_events->{$_}}, [$pos - length($last_expression), length($last_expression), $str];
-        }
-    }
-    last READ if $pos >= $length;
-    eval {$pos = $recce->resume(); };
-    last READ if $@;
-  } ## end READ: while (1)
-
-  print "Actual events: ",Dumper($actual_events);
-  my $final = {"status" => "OK",
-               "payload" => $actual_events,
-               "message" => encodeURIComponent($input)  };
-  my $json = encode_json $final;
-  # my$str={"status":"OK","payload":[],"message":"No obvious problems."}
-  $self->res->headers->header('Access-Control-Allow-Origin' => '*');
-  $self->render(text=> $json);
-};
-
-post '/get_arguments' => sub {
-  my $self = shift;
-  my @post_params = $self->req->body_params->pairs || [];
-  print "POST PARAMS = \n";
-  p @post_params;
-  print "\nEND_POST_PARAMS\n";
-  # my $input = $post_params[0][1];
-  my $input = $global_input;
-
-  p $input;
-  # my $input = $input3; 
-  my $temp = $input;
-  #print "\$input =$ input \n";
-  #Take away whitespaces around tags
-  $input =~ s/\s*(<\/?[^<>\s]*(?:\s*[^=<>]+\s*="[^"]*"\s*)*>)\s*/$1/g;
-  print "Trimmed input: \n$input \n";
-  p $input;
-  my $input_len = length $input;
-  print "Length of input: $input_len \n";
-  
-  ## GET ARGUMENT POSITIONS
-  my $name = $post_params[0][3];
-  my $start = $post_params[0][5];
-  my $length = $post_params[0][7];
-  
-  my $result = {};
   my $end = $start + $length;
-   
-  my $recce = Marpa::R2::Scanless::R->new({ grammar => $grammar, semantics_package => 'My_Actions' } )  ;
-  my $pos = $recce->read(\"$input", $start, $length);
-  
-  while ($pos < $end) {
-     eval { $pos = $recce->resume(); };
-  }
+  while ($pos < $end) { eval { $pos = $recce->resume(); }; }
 
+  my $payload = {};
   my $counter = 0;
   my $valueList = ();
   my $value_ref = \'is defined';
-  my $t0 = [gettimeofday];#
+  my $t0 = [gettimeofday];
+  print "BEFORE LOOP\n";
   PROCESS: while (defined $value_ref) {
     $counter++;
     last PROCESS if $counter>100;  #BEST VALUE TO BE DETERMINED
@@ -175,44 +109,46 @@ post '/get_arguments' => sub {
       my $actual_value = ${$value_ref};
 
       my $notations = getNotations($actual_value);
+      print "NOTATIONS\n";
       p $notations;
-      if (%$notations) {   
-        foreach (@{$notations->{$name}}) { 
-          my $el = $_;
-          my $s = $el->{'position'}->[0]->[0];
-          my $l = $el->{'position'}->[0]->[1];
-          my $flag = 0;
-          if ($s ==   $start && $l ==  $length) {
-              foreach (@{$result->{$name}}) {
-                $flag = 1 if (Compare($_,$el)==1); 
+      if (%$notations) {
+        while (my ($k, $values) = each %$notations) {
+          foreach(@$values){
+              my $v = $_;
+              print "KEY\n";
+              p $k;
+              print "VALUE\n";
+              p $v;
+              print "ENDVAL\n";
+              my $flag = 0;
+              foreach (@{$payload->{$k}}) {
+                $flag = 1 if (Compare($_, $v) == 1);
               }
-              push @{$result->{$name}}, $_ if ($flag == 0);
+              push @{$payload->{$k}}, $v if $flag == 0;
           }
-        }
+        }  
       }
     }
   }
-  #Print results of /get_arguments
-  print "Get_arguments:";
-  p $result;
+
+  p $payload;
   my $final = {"status" => "OK",
-               "payload" => $result->{$name},
-               "key" => $name,
-               "message" => encodeURIComponent($input) };
+               "payload" => $payload,
+               "message" => encodeURIComponent($input)  };
   my $json = encode_json $final;
-  #my $str={"status":"OK","payload":[],"message":"No obvious problems."}
+  # my$str={"status":"OK","payload":[],"message":"No obvious problems."}
   $self->res->headers->header('Access-Control-Allow-Origin' => '*');
   $self->render(text=> $json);
 };
 
-#FUNCTION FOR NOTATION/ARGUMENT PROCESSING
+#FUNCTION FOR NOTATION/ARGUMENT PROCESSING ############################
 sub getNotations {
   my ($rule) = @_;
   my $name = $rule->[0];
   my $result = {};
   if ($name =~ /N\d+$/) {
     my $attrib = extractArgs($rule);
-    $attrib->{"position"} = [[$rule->[1],$rule->[2]]]; 
+    $attrib->{"position"} = [[$rule->[1],$rule->[2], encodeURIComponent(substr($global_input, $rule->[1], $rule->[2]))]]; 
 
     push @{$result->{$name}}, $attrib; 
   }
@@ -243,25 +179,3 @@ sub extractArgs {
 }
 
 app->start;
-
-# my $final = {"status" => "OK",
-#              "payload" =>  
-#              {
-#                          '_pair_twodimN600' => [
-#                                                {
-#                                                  'position' => [
-#                                                                  [
-#                                                                    49,
-#                                                                    33
-#                                                                  ]
-#                                                                ],
-#                                                  'argRuleN600A1Arg' => [
-#                                                                        [
-#                                                                          55,
-#                                                                          10
-#                                                                        ]
-#                                                                      ]
-#                                                }
-#                                              ]
-#             },
-#              "message" => "No obvious problems"};
